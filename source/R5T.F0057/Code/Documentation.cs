a45475d5fad3fb128aa69a0e53bcb14d4a250d89
davidcoats <david@getsafetycone.com>
2022-10-09 19:52:22 -0700

    Initial commit.

---
+++ source/R5T.F0057/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0057
+{
+	/// <summary>
+	/// Repository paths functionality (RepositoryPathsOperator).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0057/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0057
+Repository paths functionality (RepositoryPathsOperator).
